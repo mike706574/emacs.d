d6937730e7b0f5bb90365bb354052074bd71d03c
EAS7510 <michaeleaster@northwesternmutual.com>
2018-04-14 15:39:25 -0500

    XML mode for .csproj. Setup python.

--- customizations/setup-fsharp.el
+++ customizations/setup-fsharp.el
@@ -2,3 +2,4 @@
 (add-to-list 'auto-mode-alist '("\\.fs[iylx]?$" . fsharp-mode))
 (setq inferior-fsharp-program "/usr/bin/fsharpi --readline-")
 (setq fsharp-compiler "/usr/bin/fsharpc")
+(add-to-list 'auto-mode-alist '("\\.csproj$" . xml-mode))
---
+++ customizations/setup-python.py
@@ -0,0 +1,4 @@
+(setq python-shell-interpreter "ipython"
+      python-shell-interpreter-args "-i --simple-prompt")
+
+(elpy-enable)
